ba759cd231145c168e7c8135b803833e602810ae
Benny <techiebenny@outlook.com>
2022-06-19 21:20:51 +0530

    Converts polyline to LatLon points
    Has the obtained polyline converted to array of points
    
    Co-Authored-By: Anushka Mittal <55237170+anushkamittal20@users.noreply.github.com>

--- js/path.js
+++ js/path.js
@@ -1,16 +1,21 @@
+
 var amb = {lat:13.0147,lng:77.5810};
 var rit = {lat:13.0306,lng:77.5649};
 var user_location={lat:13.0326,lng:77.5697};
 var waypoint={lat:13.0328,lng:77.5697};
 var selectElement=document.getElementById("end");
 var map=0,marker_user=0,popup=0;
+var routeResult,rawpolyline,decodedPolyline;
+
+
+
 
 function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     map = new google.maps.Map(document.getElementById("map"), {
       zoom: 15,
-      center: { lat:13.0306,lng:77.5649}, //coordinates of rit
+      center: {lat:13.0306,lng:77.5649}, //coordinates of rit
     });
 
 
@@ -37,6 +42,23 @@ function initMap() {
       })
       .then((response) => {
         directionsRenderer.setDirections(response);
+
+        //
+        console.log("Display routeResult = routes[0]")
+        routeResult = response.routes[0];
+        console.log(routeResult)
+
+ 
+        console.log("Display polyline")
+        rawPolyline = routeResult.overview_polyline;
+        console.log(rawPolyline)
+        
+        decodedPolyline = decodePath(rawPolyline);
+        console.log(decodedPolyline,5);
+        // var polyline_returned = decodePath(routeResult.overviewPolyline,5);
+        // console.log(polyline_returned)
+ 
+
       })
       .catch((e) => window.alert("Directions request failed due to " + status));
   }
@@ -61,9 +83,11 @@ function initMap() {
   },10000);
   }
 
+
+
   selectElement.addEventListener('change', (event) => { 
     var x=selectElement.value
-    if(x=="{13.0328,77.5697}")
+    if(userLocatedWithinRadius(user_location, waypoint, 2000))
     {
         //setTimeout(PlaceMarker(user_location),10000);
         PlaceMarker(user_location);
@@ -89,3 +113,52 @@ function removeUser(){
 
 }
   
+
+
+
+function decodePath(str, precision) {
+  var index = 0,
+      lat = 0,
+      lng = 0,
+      coordinates = [],
+      shift = 0,
+      result = 0,
+      byte = null,
+      latitude_change,
+      longitude_change,
+      factor = Math.pow(10, Number.isInteger(precision) ? precision : 5);
+
+
+  while (index < str.length) {
+
+      // Reset shift, result, and byte
+      byte = null;
+      shift = 0;
+      result = 0;
+
+      do {
+          byte = str.charCodeAt(index++) - 63;
+          result |= (byte & 0x1f) << shift;
+          shift += 5;
+      } while (byte >= 0x20);
+
+      latitude_change = ((result & 1) ? ~(result >> 1) : (result >> 1));
+
+      shift = result = 0;
+
+      do {
+          byte = str.charCodeAt(index++) - 63;
+          result |= (byte & 0x1f) << shift;
+          shift += 5;
+      } while (byte >= 0x20);
+
+      longitude_change = ((result & 1) ? ~(result >> 1) : (result >> 1));
+
+      lat += latitude_change;
+      lng += longitude_change;
+
+      coordinates.push([lat / factor, lng / factor]);
+  }
+
+  return coordinates;
+};
