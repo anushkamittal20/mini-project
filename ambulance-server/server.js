a20657a442a05ebf3c7b1ccb7585dee535f08161
Anusha Chaturvedi <techiebenny@outlook.com>
2022-07-06 00:12:01 +0530

    fixed CORS error for server
    allows to make POST requests to server now

--- ambulance-server/server.js
+++ ambulance-server/server.js
@@ -1,14 +1,51 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+let cors = require('cors')
+app.use(cors())
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+
+app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send('hello!!');
+    res.send('hey there!!');
+    // res.json.apply({info: 'hey'})
 })
 
 
 app.get('/info',(req,res)=>{
+    // res.send('hey there!!');
     res.status(200).json({info: 'hey'})
 })
+
+app.post('/',(req,res)=>{
+    // const { parcel } = req.body
+    var src = req.body.source
+    var dest = req.body.destination
+    console.log('source : ',src);
+    console.log('destination : ',dest);
+    res.status(200).send({status: 'recieved',
+        source: src,
+        destination: dest  
+    })
+})
+
+app.post('/ambulance/routes',async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+// app.put('/ambulance/:ambulanceID')
+
+
 
 
 const PORT = process.env.PORT || 8080;
