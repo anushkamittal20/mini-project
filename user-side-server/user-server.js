84b56170260c183ea8912be18c8e2aa01b3f3539
Anushka Mittal <anumittal4641@gmail.com>
2022-07-06 12:37:08 +0530

    Database conn; user side refactor

--- user-side-server/user-server.js
+++ user-side-server/user-server.js
@@ -1,10 +1,28 @@
 const express = require('express')
+const redis = require('redis')
 const app=express()
 const port = 8383
+const client = redis.createClient({
+    url: 'redis://YOUR REDIS INSTANCE URL'
+    //need this from benny
+  })
+
+  async function connect_db() {
+    await client.connect()
+    const amb_wp = await client.get("next_wp")
+  }
+
+  async function db_get_wp() {
+   return amb_wp = await client.get("next_wp")
+  }
 
 app.get('/',(req,res) => {
     res.status(200).send('<h1>Functionalities for user-server communication</h1>')
 })
+
+function hello(){
+console.log("hello server")
+}
 
 
 
